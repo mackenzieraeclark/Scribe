a32e5a99aabf581e7a4b3301f22452cec79d8bba
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-10-07 00:00:45 -0400

    route to html files

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Set Dependencies
-
 let express = require("express");
 let path = require("path");
 
@@ -11,3 +10,13 @@ let notesDB = require('./db/db.json');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Route to HTML docs
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
