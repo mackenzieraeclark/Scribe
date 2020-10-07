7fef02cc86a5839362361e635eb55057832d5aa9
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-10-06 23:55:51 -0400

    set up express app

--- server.js
+++ server.js
@@ -2,3 +2,12 @@
 
 let express = require("express");
 let path = require("path");
+
+// Set up Express App
+let app = express();
+let PORT = process.env.PORT || 3000;
+let notesDB = require('./db/db.json');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
