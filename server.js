7010757e98bab9cbc2b3d4c8d7ee7bf89fcdc916
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-10-07 00:15:31 -0400

    api route

--- server.js
+++ server.js
@@ -13,12 +13,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Route to HTML docs using GET
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/notes", function(req, res) {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// Route to API
+app.get("/api/notes", (req, res) => {
+    res.json(notesDB);
 });
 
 // POST new note
